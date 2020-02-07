a071dea6a07db043d4889fe1e7aa302f78ee8558
Jonathan Abrahams <jsja20@bath.ac.uk>
2020-02-07 16:59:44 +0000

    Fixed different ENA URL

--- bin/ENA_download.R
+++ bin/ENA_download.R
@@ -11,6 +11,9 @@ args = commandArgs(trailingOnly=TRUE)
 
 First_6=substr(args[1],1,6)
 lenny=nchar(args[1])
+if(lenny>=10)
+{
+
 Last_1_modified=paste("00",substr(args[1],lenny,lenny),sep="")
 
 full_url_1=paste("ftp://ftp.sra.ebi.ac.uk/vol1/fastq",First_6,Last_1_modified,args[1],paste(args[1],"_1.fastq.gz",sep=""),sep="/")
@@ -21,3 +24,20 @@ print(paste("First call: ","wget",full_url_2,"-P","./Data/"))
 system(paste("wget",full_url_1,"-P","./Data/"))
 
 system(paste("wget",full_url_2,"-P","./Data/"))
+}
+
+if(lenny<10)
+{
+
+full_url_1=paste("ftp://ftp.sra.ebi.ac.uk/vol1/fastq",First_6,args[1],paste(args[1],"_1.fastq.gz",sep=""),sep="/")
+full_url_2=paste("ftp://ftp.sra.ebi.ac.uk/vol1/fastq",First_6,args[1],paste(args[1],"_2.fastq.gz",sep=""),sep="/")
+print(paste("First call: ","wget",full_url_1,"-P","./Data/"))
+print(paste("First call: ","wget",full_url_2,"-P","./Data/"))
+
+system(paste("wget",full_url_1,"-P","./Data/"))
+
+system(paste("wget",full_url_2,"-P","./Data/"))
+
+
+}
+
